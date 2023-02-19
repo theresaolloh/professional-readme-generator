35a6f517e2bf925397307efef8ea794f8dadcd30
theresao <theresaolloh@gmail.com>
2023-02-19 12:28:54 +0000

    first commit

---
+++ app.js
@@ -0,0 +1,12 @@
+const inquirer = require('inquirer')
+
+
+//list of questions for thr user to fill
+const questions = [
+    {
+        type: 'input',
+        name: 'title',
+        message: 'What is the title of your project?',
+    }
+]
+
