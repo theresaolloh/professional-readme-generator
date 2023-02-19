819dbf8eaa9b4535ca9bbcd4f89c5b58e0566121
theresao <theresaolloh@gmail.com>
2023-02-19 12:36:54 +0000

    added more questions

--- app.js
+++ app.js
@@ -7,6 +7,21 @@ const questions = [
         type: 'input',
         name: 'title',
         message: 'What is the title of your project?',
+    },
+    {
+        type: 'input',
+        name: 'description',
+        message: 'What is your project about? Give a brief description of what your project does',
     }
+    {
+        type: 'input',
+        name: 'Table of Content',
+        message: 'Table of Content',
+    },
+    {
+        type: 'input',
+        name: 'Installation',
+        message: 'What does a user need to install to utilise your prject?',
+    },
 ]
 
