802e8067a5e2efe1c00a8a5385c4f2f7afd38e23
theresao <theresaolloh@gmail.com>
2023-02-19 14:27:10 +0000

    added all the questions

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
-const inquirer = require('inquirer')
-
+//const inquirer = require('inquirer');
+import inquirer from "inquirer"
 
 //list of questions for thr user to fill
 const questions = [
@@ -12,7 +12,7 @@ const questions = [
         type: 'input',
         name: 'description',
         message: 'What is your project about? Give a brief description of what your project does',
-    }
+    },
     {
         type: 'input',
         name: 'Table of Content',
@@ -21,7 +21,50 @@ const questions = [
     {
         type: 'input',
         name: 'Installation',
-        message: 'What does a user need to install to utilise your prject?',
+        message: 'What does a user need to install to utilise your project?',
+    },
+    {
+        type: 'input',
+        name: 'Instructions',
+        message: 'How should your app/software be used?',
+    },
+    {
+        type: 'input',
+        name: 'License',
+        message: 'License to use',
+    },
+    {
+        type: 'input',
+        name: 'Contributions',
+        message: 'Who contributed to this project?',
+    },
+    {
+        type: 'input',
+        name: 'Tests',
+        message: 'How will this project be tested?',
+    },
+    {
+        type: 'input',
+        name: 'Contact',
+        message: 'What is you email? How can you be reached for inquiries?',
+    },
+    {
+        type: 'input',
+        name: 'GitHub',
+        message: 'What is your GitHub username?',
     },
 ]
 
+//the 
+function init() {
+    return inquirer.prompt(questions)
+        .then((answers) => {
+            console.log(answers)
+            return answers
+        })
+        .catch((error) => {
+            console.log(error)
+        })
+}
+
+init()
